b3d852e56ae39edd7866bbb1fada743a7cc4a5c1
daijihong <1092104983@qq.com>
2025-10-27 10:58:04 +0800

    update json example; bug fix: visit docs url; bug fix:catch async error

--- http_server.py
+++ http_server.py
@@ -13,7 +13,7 @@ from pydantic import create_model
 from dotenv import load_dotenv
 from aiofiles import open as aopen
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Body
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
@@ -24,8 +24,9 @@ from logg import logger, LOGGER_DIR, WORK_DIR
 from schemas import SendMessage, HttpMessageStatus, HttpMessageStatusBase
 from tools import async_wrapper,send_stable,b64decode, DB_Client
 
-load_dotenv(dotenv_path=WORK_DIR / ".env")
+load_dotenv(dotenv_path=WORK_DIR / ".env", override=True)
 WAIT_BEFORE_REFRESH=os.getenv("WAIT_BEFORE_REFRESH",5)
+print(f"WAIT_BEFORE_REFRESH: {WAIT_BEFORE_REFRESH}")
 WAIT_BEFORE_REFRESH=float(WAIT_BEFORE_REFRESH)
 
 chatbot_client = CmccChatClient(cache_session_map=False, wait_before_refresh=WAIT_BEFORE_REFRESH)
@@ -41,7 +42,7 @@ async def execute_send_message():
     global db_client
     while True:
         message:SendMessage = await message_queue.get()
-        message_id = message.id
+        message_id = str(message.id)
         send_to = message.FromWxid
 
         async with message_semaphore:
@@ -63,19 +64,23 @@ async def execute_send_message():
                     )
                 except Exception as exc:
                     logger.error(traceback.format_exc())
-                    message_status = HttpMessageStatus(
+                    message_status = HttpMessageStatusBase(
                         message_id=message_id,
                         send_to=send_to,
                         content=content,
                         success=False, failure_reason=str(exc))
                 else:
-                    message_status = HttpMessageStatus(
+                    message_status = HttpMessageStatusBase(
                         message_id=message_id,
                         send_to=send_to,
                         content=content,
                         success=True)
-                result = await db_client.create(HttpMessageStatusBase, message_status, )
 
+                try: #NOTE needs to catch error here, else asyncio task ignores it and keeps go on.
+                    result = await db_client.create(message_status, HttpMessageStatus)
+                    await async_wrapper(logger.info, f"[text message sent] {message_id}")
+                except Exception as e:
+                    raise Exception(e) from e
             #NOTE send file if exists
             if message.File:
                 try:
@@ -94,18 +99,22 @@ async def execute_send_message():
                     )
                 except Exception as exc:
                     logger.error(traceback.format_exc())
-                    message_status = HttpMessageStatus(
+                    message_status = HttpMessageStatusBase(
                         message_id=message_id,
                         send_to=send_to,
                         content=content,
                         success=False, failure_reason=str(exc))
                 else:
-                    message_status = HttpMessageStatus(
+                    message_status = HttpMessageStatusBase(
                         message_id=message_id,
                         send_to=send_to,
                         content=content,
                         success=True)
-                result = await db_client.create(HttpMessageStatusBase, message_status, )
+                try: #NOTE needs to catch error here, else asyncio task ignores it and keeps go on.
+                    result = await db_client.create(message_status, HttpMessageStatus)
+                    await async_wrapper(logger.info, f"[file message sent] {message_id}")
+                except Exception as e:
+                    raise Exception(e) from e
 
             #XXX mark task done
             message_queue.task_done()
@@ -116,6 +125,7 @@ async def lifespan(app: FastAPI):
     global consumer_tasks, db_client
     await async_wrapper(logger.info, "create table")
     db_client = DB_Client()
+    await db_client.migrate()
 
     #NOTE create 4 consumers, though number of processes is limited to 1 by semaphore.
     for i in range(4):
@@ -128,6 +138,7 @@ async def lifespan(app: FastAPI):
         t.cancel()
     await asyncio.gather(*consumer_tasks, return_exceptions=True)
     rmtree(temp_dir, ignore_errors=True) #NOTE remove all files in temp dir
+    await logger.complete() #NOTE complete all logs
     logger.info("[STATUS] successsfully shuting down server")
 
 
@@ -139,11 +150,12 @@ app.add_middleware(
     allow_methods=["*"],  # 允许的请求方法列表，这里使用通配符表示支持所有方法
     allow_headers=["*"],  # 允许的请求头列表，这里使用通配符表示支持所有头部字段
 )
-app.mount("/statics/",StaticFiles(directory="./statics"), "static")
+app.mount("/statics",StaticFiles(directory="./statics"), "statics")
 
 @app.get("/docs", include_in_schema=False)
 async def custom_swagger_docs():
     return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
         title="Swagger",
         swagger_js_url="/statics/swagger-ui-bundle.js",
         swagger_css_url="/statics/swagger-ui.css"
@@ -152,6 +164,7 @@ async def custom_swagger_docs():
 @app.get("/redoc", include_in_schema=False)
 async def custom_redoc():
     return get_redoc_html(
+        openapi_url=app.openapi_url,
         title="Redoc",
         redoc_js_url="/statics/redoc.standalone.js"
     )
@@ -168,20 +181,35 @@ async def check_message_status(
     if_empty = True
     async for result in db_client.get(HttpMessageStatus, message_id=message_id):
         if_empty=False
-        return JSONResponse(content=dict(message_status=result, empty=if_empty))
+        return JSONResponse(content=dict(message_status=result.model_dump(mode="json"), empty=if_empty))
     if if_empty:
         return JSONResponse(content=dict(message_status=None, empty=if_empty))
 
 
+json_schemas_example={
+    "Business": None,
+    "Content": "",
+    "FromWxid": "",
+    "ActualName": "",
+    "Role": "",
+    "SenderWxid": "",
+    "File": None,
+    "Filename": None,
+    "id": None,
+    "CreatedTime": None,
+    "IsSent": None,
+    "SendTime": None
+}
+
 @app.post("/receive_message/",)
-async def receive_message(message:SendMessage):
+async def receive_message(message:SendMessage=Body(..., example=json_schemas_example)):
     message_id = message.id
     await message_queue.put(message)
     return JSONResponse(
         content={
             "status":200,
             "message":"message received. You can use message_id to check if your message is sent properly.",
-            "message_id":message_id},
+            "message_id":str(message_id)},
     )
 
 
